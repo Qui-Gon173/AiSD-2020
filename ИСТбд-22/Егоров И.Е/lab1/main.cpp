36397d04d6bafd8ef513b057ee24d0c8d03e64f9
MONSTER-PC\Voodoo <doomaykaka@gmail.com>
2020-10-29 14:49:24 +0400

    lab1

--- ИСТбд-22/Егоров И.Е/lab1/main.cpp
+++ ИСТбд-22/Егоров И.Е/lab1/main.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 int main () {
 
-cout<<"Algoritm lineinogo poiska"<<endl;
+cout<<"Algoritm lineinogo poiska c barierom"<<endl;
 cout<<endl;
 
 cout<<"Vvedite chislo elementov"<<endl;
 int num;
 cin>>num;
-int m[num];
+int m[num+1];
 for(int i=0;i<num;i++){
  m[i]=rand()%(9-0);
 }
@@ -25,11 +25,17 @@ cout<<endl;
 cout<<"Vvedite kluch"<<endl;
 int key;
 cin>>key;
-for(int i=0;i<num;i++){
- if(m[i]==key){
-     cout<<"element c indexom "<<i<<" podhodit usloviu lineinogo poiska"<<endl;
+m[num+1]=key;
+int i=0;
+while(m[i]!=key){
+       i++; 
  }
-}
+if(i==num+1){
+cout<<"v masive element ne naiden"<<endl;
+   }else{
+       cout<<"element c indexom "<<i<<" podhodit usloviu lineinogo poiska"<<endl;
+   }
+
 system("pause");
 return 0;
 }
