41e8f72fc2669bd9900e045588572e583b66e743
R4mpage <theseconddream2@bk.ru>
2020-11-01 22:57:57 +0400

    Add an array length for u

--- ISTbd-21/AbeluanovDM/lab4/main.cpp
+++ ISTbd-21/AbeluanovDM/lab4/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
 int main() {
-    int array[10];
-    for (int i = 0; i < 10; i++) {
+    int n;
+    std::cout << "Array length: ";
+    std::cin >> n;
+    int array[n];
+    for (int i = 0; i < n; i++) {
         std::cin >> array[i];
     }
 
     std::cout << std::endl;
     
-    for (int j = 0; j < 10; j++) {
-        for (int i = 10 / 2 - 1 - j / 2; i > -1; i--) {
-            if (2 * i + 2 <= 10 - 1 - j) {
+    for (int j = 0; j < n; j++) {
+        for (int i = n / 2 - 1 - j / 2; i > -1; i--) {
+            if (2 * i + 2 <= n - 1 - j) {
                 if (array[2 * i + 1] > array[2 * i + 2]) {
                     if (array[i] < array[2 * i + 1]) {
                         int temp = array[i];
@@ -27,7 +30,7 @@ int main() {
                 }
             }
             else {
-                if (2 * i + 1 <= 9 - j) {
+                if (2 * i + 1 <= n - 1 - j) {
                     if (array[i] < array[2 * i + 1]) {
                         int temp = array[i];
                         array[i] = array[2 * i + 1];
@@ -37,11 +40,11 @@ int main() {
             }
         }
         int temp = array[0];
-        array[0] = array[9 - j];
-        array[9 - j] = temp;
+        array[0] = array[n - 1 - j];
+        array[n - 1 - j] = temp;
     }
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < n; i++) {
         std::cout << array[i] << " ";
     }
     std::cout << std::endl;
