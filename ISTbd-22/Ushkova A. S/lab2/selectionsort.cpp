06b63dde1dab8211bd592de770f4809e0290b4a5
LAPTOP-VNO8FJGM\HP <s.ushkva@yandex.ru>
2020-11-03 12:51:05 +0400

    lab 2 исправленная

--- ISTbd-22/Ushkova A. S/lab2/selectionsort.cpp
+++ ISTbd-22/Ushkova A. S/lab2/selectionsort.cpp
@@ -2,25 +2,34 @@
 
  using namespace std;
 
+void SelectionSort(int array[], int k)
+{
+  int i,j, min, temp;
+  for(i=0;i<k-1; i++)
+  {
+    min=i;
+    for (j=i+1;j<k;j++)
+    {
+      if(array[j]<array[min])
+      min=j;
+    }
+    if (min!=i)
+    {
+    std::swap(array[i], array[min]);
+    min=i;
+    }
+  }
+}
+
 int main()
 {
   int length=8;
   int array[length]={7,0,8,19,3,8,2,6};
   cout<<" Sourse array "<<endl;
   for(length=0; length<8; ++length)
-  std::cout << array[length] << ' ';
-  
-    for (int startIndex=0; startIndex<length-1; startIndex++)
-    {
-        int smallestIndex = startIndex;
-        for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
-        {
-            if (array[currentIndex] < array[smallestIndex])
-            smallestIndex = currentIndex;
-        }
-        std::swap(array[startIndex], array[smallestIndex]);
-    }
+  {std::cout << array[length] << ' ';}
+   SelectionSort(array,length);
    cout<<"\n Sorted array "<<endl;
-   for (int index = 0; index < length; ++index)
-		std::cout << array[index] << ' ';
+   for (int i = 0; i < length; ++i)
+		{std::cout << array[i] << ' ';}
 }
