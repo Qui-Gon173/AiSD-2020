2071e6599dbcb41301d7fab25c5318168e33003c
v01d <v01dmain@aol.com>
2020-11-03 15:45:03 +0400

    Add lab6

---
+++ ISTbd-21/KoltsovAA/lab6/code_1.cpp
@@ -0,0 +1,20 @@
+/** 
+ * Выбор данных из памяти с помощью разных указателей
+ * Использование функций приведения типов
+ */
+
+#include <iostream>
+
+
+int main() {
+    unsigned long L = 12345678;
+    char *cp = (char *)&L;
+    int *ip = (int *)&L;
+    long *lp = (long *)&L;
+
+    std::cout << "\n&L = " << &L;
+    std::cout << "\nL = " << L;
+    std::cout << "\n*cp = " << *cp;
+    std::cout << "\n*ip = " << *ip;
+    std::cout << "\n*lp = " << *lp;
+}
