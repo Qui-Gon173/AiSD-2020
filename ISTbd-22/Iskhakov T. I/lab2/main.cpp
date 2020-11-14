1a3c95ed9d986eba4b90df1b3d9e9119b1d68538
Timur Iskhakov <ksewnv@gmail.com>
2020-11-14 15:17:41 +0400

    add lab 2

---
+++ ISTbd-22/Iskhakov T. I/lab2/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintArray(int array[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i] << endl;
+	}
+}
+
+// Сортировка методом перебора
+
+void SelectionSort(int array[], int size)
+{
+	int minIndex, temp;
+	for (int i = 0; i < size - 1; i++)
+	{
+		minIndex = i;
+		for (int j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minIndex])
+				minIndex = j;
+		}
+		temp = array[i];
+		array[i] = array[minIndex];
+		array[minIndex] = temp;
+	}
+}
+
+int main()
+{
+	const int SIZE = 10;
+	int array[SIZE] = {12, 44, 59, 3, 16, 40, 97, 24, 0, -1};
+	PrintArray(array, SIZE);
+	SelectionSort(array, SIZE);
+	cout << " After using sort " << endl;
+	PrintArray(array, SIZE);
+	return 0;
+}
