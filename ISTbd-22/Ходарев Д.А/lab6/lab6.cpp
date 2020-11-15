f80ef19e767255802237898ead7c8377ef850b09
Danila <hodarevd@icloud.com>
2020-11-15 13:06:52 +0400

    fixed

---
+++ ISTbd-22/Ходарев Д.А/lab6/lab6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int x = 10;
+    int y = 10;
+    int* xptr = &x;
+    int* yptr = &y;
+
+    //сравниваем указатели
+    if (xptr == yptr)
+        cout << "xptr = yptr\n";
+    else
+        cout << "xptr != yptr\n";
+
+    //сравниваем значения, на которое указывает указатель
+    if (*xptr == *yptr) {
+        cout << "*xptr = *yptr\n";
+    }
+    else {
+        cout << "*xptr != *yptr\n";
+    }
+    return 0;
+}
