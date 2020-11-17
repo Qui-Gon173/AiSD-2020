328f3236f49000860e7a3329db3c0ab1aaa88579
good.woor <zxcasdqwe4132@mail.ru>
2020-11-17 12:18:33 +0300

    Add lab8.1

---
+++ ISTbd-21/LoginovNV/lab8.1/Turn.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "Element.h"
+
+class Turn
+{
+private:
+	// брать эл-ы будем с конца - это tail
+	// пояснение - он ближе всех к извлечению - первый в очереди
+	Element* tail;
+
+	// добавлять в начало - это head
+	// пояснение - он дальше всех от извлечения - последний в очереди
+	Element* head;
+
+	// кол-во эл-ов в контейнере
+	int turnSize;
+
+public:
+	// конструктор по умолчанию
+	Turn();
+
+	// добавить элемент(ы)
+	void pushElem(float number);
+	void pushArr(float numbers[], int count);
+
+	// просмотреть последний эл-т 
+	void checkLast();
+
+	// извлечь эл-т(ы)
+	float getElem();
+	float* getElem(int count);
+
+	// перенести N эл-ов в новую очередь
+	Turn transfer(int count);
+
+	// вывести все элементы на экран
+	void printTurn();
+
+	// очистить очередь
+	void deleteTurn();
+
+	// вывести кол-во элементов в очереди
+	int retSize()
+	{
+		return turnSize;
+	}
+
+	// вывести max, min, sum значения
+	void showCharacters();
+};
+
