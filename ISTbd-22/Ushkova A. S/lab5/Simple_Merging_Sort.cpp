13493eb6ef6ac977a6fc77edb4148d85a6dabcaa
LAPTOP-VNO8FJGM\HP <s.ushkva@yandex.ru>
2020-11-22 13:19:42 +0400

    lab 5 изменено

--- ISTbd-22/Ushkova A. S/lab5/Simple_Merging_Sort.cpp
+++ ISTbd-22/Ushkova A. S/lab5/Simple_Merging_Sort.cpp
@@ -1,106 +1,103 @@
 #include <iostream>
+#include <algorithm>
+#include <queue>
+#include <limits>
 #include <fstream>
-#include <cstdio>
-
 using namespace std;
-
-void Simple_Merging_Sort (char *name){
-  int a1, a2, k, i, j, kol;
-  FILE *f, *f1, *f2;
-  kol = 0;
-  if ( (f = fopen(name,"r")) == NULL )
-    cout<<"\n Исходный файл не может быть прочитан..."<<endl;
-  else {
-    while ( !feof(f) ) 
+//Описание функции сортировки простым слиянием
+void Simple_Merging_Sort(char* name) {
+    int a1, a2, k, i, j, kol, tmp;
+ fstream f,f1,f2;
+ f.open(name, fstream::in);
+ kol = 0;
+    if (!f)
     {
-      fscanf(f,"%d",&a1);
-      kol++;
+ cout << " Исходный файл не может быть прочитан...";
     }
-    fclose(f);
-  }
-  k = 1;
-  while ( k < kol )
-  {
-    f = fopen(name,"r");
-    f1 = fopen("smsort_1","w");
-    f2 = fopen("smsort_2","w");
-    if ( !feof(f) ) fscanf(f,"%d",&a1);
-    while ( !feof(f) )
-    {
-      for ( i = 0; i < k && !feof(f) ; i++ )
-      {
-        fprintf(f1,"%d ",a1);
-        fscanf(f,"%d",&a1);
-      }
-      for ( j = 0; j < k && !feof(f) ; j++ )
-      {
-        fprintf(f2,"%d ",a1);
-        fscanf(f,"%d",&a1);
-      }
-    }
-    fclose(f2);
-    fclose(f1);
-    fclose(f);
-
-    f = fopen(name,"w");
-    f1 = fopen("smsort_1","r");
-    f2 = fopen("smsort_2","r");
-    if ( !feof(f1) ) fscanf(f1,"%d",&a1);
-    if ( !feof(f2) ) fscanf(f2,"%d",&a2);
-    while ( !feof(f1) && !feof(f2) )
-    {
-      i = 0;
-      j = 0;
-      while ( i < k && j < k && !feof(f1) && !feof(f2) )
-       {
-        if ( a1 < a2 )
-         {
-          fprintf(f,"%d ",a1);
-          fscanf(f1,"%d",&a1);
-          i++;
+    else {
+        while (f) {
+ f >> a1;
+ kol++;
         }
-        else
-         {
-          fprintf(f,"%d ",a2);
-          fscanf(f2,"%d",&a2);
-          j++;
+ f.close();
+    }
+ k = 1;
+
+    while (k < kol) {
+     f.open(name, fstream::in);
+     f1.open("smsort_1", fstream::out);
+     f2.open("smsort_2", fstream::out);
+        if (f) f >>a1;
+        while (f) {
+            for (i = 0; i < k && f; i++) {
+        f1 << a1 << endl;
+                f >> a1;
+            }
+            for (j = 0; j < k && f; j++) {
+
+                f2 << a1 << endl;
+                f >> a1;
+            }
         }
-      }
-      while ( i < k && !feof(f1) ) 
-      {
-        fprintf(f,"%d ",a1);
-        fscanf(f1,"%d",&a1);
-        i++;
-      }
-      while ( j < k && !feof(f2) ) 
-      {
-        fprintf(f,"%d ",a2);
-        fscanf(f2,"%d",&a2);
-        j++;
-      }
-    }
-    while ( !feof(f1) ) 
-    {
-      fprintf(f,"%d ",a1);
-      fscanf(f1,"%d",&a1);
-    }
-    while ( !feof(f2) ) 
-    {
-      fprintf(f,"%d ",a2);
-      fscanf(f2,"%d",&a2);
-    }
-    fclose(f2);
-    fclose(f1);
-    fclose(f);
-    k *= 2;
-  }
-  remove("smsort_1");
-  remove("smsort_2");
+        f1.close();
+        f.close();
+        f2.close();
+
+
+        f.open(name, fstream::out);
+        f1.open("smsort_1", fstream::in);
+        f2.open("smsort_2", fstream::in);
+
+        if (f1) f1 >> a1;
+        if (f2) f2 >> a2;
+        while (f1 && f2) {
+            i = 0;
+            j = 0;
+            while (i < k && j < k && f1 && f2) {
+                if (a1 < a2) {
+                    f << a1 <<endl;
+                   f1 >> a1;
+                    i++;
+                }
+                else {
+                    f << a2 <<endl;
+                    f2 >> a2;
+                    j++;
+                }
+            }
+            while (i < k && f1) {
+                f << a1 <<endl;
+                f1>>a1;
+                i++;
+            }
+            while (j < k && f2) {
+                f << a2 <<endl;
+               f2 >>a2;
+                j++;
+            }
+        }
+
+
+        while (f1) {
+            f << a1 <<endl;
+            f1 >> a1;
+
+        }
+        while (f2) {
+            f << a2 <<endl;
+            f2 >> a2;
+        }
+        f2.close();
+        f1.close();
+        f.close();
+        k *= 2;
 }
-
+  remove("smsort_1.txt");
+  remove("smsort_2.txt");
+}
 int main()
 {
-    
+
     Simple_Merging_Sort("text.txt");
     return 0;
 }
