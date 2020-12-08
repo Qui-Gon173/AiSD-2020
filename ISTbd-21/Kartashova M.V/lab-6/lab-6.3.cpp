38e6520f3b3cd2287ce0c433bbecfac6c6621f0e
Marish-Ka <kartmarina-73@mail.ru>
2020-12-09 01:11:59 +0300

    add lab6

---
+++ ISTbd-21/Kartashova M.V/lab-6/lab-6.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RUS");
+
+	int x = 10;
+	int y = 10;
+	int *xptr = &x;
+	int *yptr = &y;
+
+	//сравниваем указатели
+	if (xptr == yptr)
+		cout << "Указатели равны\n";
+	else
+		cout << "Указатели равны\n";
+
+	//сравниваем значения, на которое указывает указатель
+	if (*xptr == *yptr) {
+		cout << "Значения равны\n";
+	}
+	else {
+		cout << "Значения равны\n";
+	}
+	system("pause");
+	return 0;
+}
