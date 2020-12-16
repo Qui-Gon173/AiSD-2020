73c8957a57cdf266526178c6d75620b45bc22ea0
DESKTOP-SAU2O45\Иван <vanyatagashev@gmail.com>
2020-12-16 23:00:45 +0400

    fix

--- ISTbd-22/Tagashev I.I/lab8.1/list.h
+++ ISTbd-22/Tagashev I.I/lab8.1/list.h
@@ -1,266 +1,210 @@
 #pragma once
+template<typename T>
 class List
 {
-public:
-    List();
-    ~List();
-    void adding_element(unsigned long data);
-    void adding_element(int place, unsigned long data);
-    void print_from_head();
-    void print_from_end();
-    int getsize() { return count_elements; };
-    void delete_target_element(int target);
-    void search_index();
-    void cls();
-    void search_index(unsigned long value);
-    unsigned long sum();
-    unsigned long mid_value();
-    unsigned long min_value();
-    unsigned long max_value();
-    unsigned long& operator[](const int index);
 private:
+	int size = 0;
+	template<typename T>
+	class Node
+	{
+	public:
+		T data;
+		Node<T>* next;
+		Node<T>* prev;
 
-    class Node
-    {
-    public:
-        Node* pNext;
-        Node* pPrev;
-        unsigned long data;
-        Node(unsigned long data = 123, Node* pPrev = nullptr, Node* pNext = nullptr)
-        {
-            this->data = data;
-            this->pPrev = pPrev;
-            this->pNext = pNext;
-        }
-    };
-    int count_elements;
-    Node* head;
-    Node* tail;
+		Node()
+		{
+			prev = nullptr;
+			next = nullptr;
+			T = nullptr;
+		}
+		Node(T data, Node<T>* prev = nullptr, Node<T>* next = nullptr)
+		{
+			this->data = data;
+			this->next = next;
+			this->prev = prev;
+		}
+		Node<T>* get_prev()
+		{
+			return this->prev;
+		}
+		Node<T>* get_next()
+		{
+			return this->next;
+		}
+		T get_data()
+		{
+			return this->data;
+		}
+
+	};
+	Node<T>* head;
+	Node<T>* tail;
+public:
+	List()
+	{
+		size = 0;
+		head = nullptr;
+		tail = nullptr;
+	}
+	int get_size()
+	{
+		return this->size;
+	}
+
+
+	void append(T data)
+	{
+		if (size == 0)
+		{
+			head = new Node<T>(data);
+			tail = head;
+		}
+		else
+		{
+			Node<T>* tmp = new Node<T>(data);
+			tail->next = tmp;
+			tail->next->prev = tail;
+			tail = tmp;
+
+		}
+		size++;
+	}
+	void insert(T data, int ind)
+	{
+		if (size == 0)
+		{
+			head = new Node<T>(data);
+			tail = head;
+		}
+		else
+		{
+
+			Node<T>* tmp = new Node<T>(data);
+			Node<T>* Current = head;
+			for (int i = 0; i < ind - 1; i++)
+			{
+				Current = Current->next;
+			}
+			Current->prev->next = tmp;
+			tmp->prev = Current->prev;
+			tmp->next = Current;
+			Current->prev = tmp;
+		}
+		size++;
+	}
+	void delete_ind(int ind)
+	{
+		if (size == 0)
+		{
+			Node<T>* Current = head;
+			Node<T>* Buff = head;
+			Buff = Current->next;
+			delete head;
+			head = Buff;
+		}
+		else
+		{
+			Node<T>* Current = head;
+			for (int i = 0; i < ind - 1; i++)
+			{
+				Current = Current->next;
+			}
+			if (Current == head)
+				head = Current->next;
+			Current->prev->next = Current->next;
+			Current->next->prev = Current->prev;
+			delete Current;
+		}
+		size--;
+	}
+	void delete_front()
+	{
+		while (size != 0)
+		{
+			Node<T>* Current = head;
+			Node<T>* Buff = head;
+			Buff = Current->next;
+			delete head;
+			head = Buff;
+			size--;
+		}
+	}
+	int search_ind(T data)
+	{
+		//Node<T>* Current = head;
+		int ind = 0;
+		for (Node<T>* Current = head; Current != nullptr; Current = Current->next)
+		{
+			if (Current->data == data)
+			{
+				return ++ind;
+				//std::cout << "?????? ???????? ??-?? " << ++ind << std::endl;
+			}
+			ind++;
+		}
+
+	}
+	void print_list()
+	{
+		Node<T>* Current = head;
+		if (size == 0)
+		{
+			std::cout << "Ñïèñîê ïóñò." << std::endl << std::endl;
+		}
+		else
+		{
+			while (Current != nullptr)
+			{
+				std::cout << Current->get_data() << " ";
+				Current = Current->next;
+			}
+
+		}
+
+	}
+	void print_list_reverse()
+	{
+		Node<T>* Current = tail;
+
+		if (size == 0)
+		{
+			std::cout << "Ñïèñîê ïóñò." << std::endl << std::endl;
+		}
+		else
+		{
+			while (Current != nullptr)
+			{
+				std::cout << Current->get_data() << " ";
+				Current = Current->prev;
+
+			}
+
+		}
+
+	}
+	void properties_list()
+	{
+		T sum = 0, avg = 0, min, max;
+		min = max = tail->get_data();
+
+		for (Node<T>* Current = tail; Current != nullptr; Current = Current->get_prev())
+		{
+			sum += Current->get_data();
+			if (Current->get_data() > max)
+			{
+				max = Current->get_data();
+			}
+			if (Current->get_data() < min)
+			{
+				min = Current->get_data();
+			}
+		}
+		avg = sum / size;
+		std::cout << "max = " << max << std::endl;
+		std::cout << "min = " << min << std::endl;
+		std::cout << "sum = " << sum << std::endl;
+		std::cout << "avg = " << avg << std::endl;
+	}
+
 };
-
-List::List()
-{
-    count_elements = 0;
-    head = nullptr;
-}
-// Ð”ÐµÑÑ‚Ñ€ÑƒÐºÑ‚Ð¾Ñ€
-List::~List()
-{
-}
-unsigned long& List::operator[](const int index)
-{
-    int counter = 0;
-    Node* current = this->head;
-    while (current != nullptr)
-    {
-        if (counter == index) {
-            return current->data;
-        }
-        current = current->pNext;
-        counter++;
-    }
-
-}
-
-void List::adding_element(unsigned long data)
-{
-    if (head == nullptr)
-    {
-        head = new Node(data);
-        Node* current = head;
-        std::cout << "[HEAD]" << " || " << "[pPrev]: " << current->pPrev << " || " << "[ADDRESS]: " << current << " || " << "[DATA]: " << data << " || " << "[pNext]:" << current->pNext << std::endl;
-    }
-    else
-    {
-        Node* current = this->head;
-        if (count_elements == 1) {
-            Node* current_this;
-            current->pNext = new Node(data, current);
-            tail = current->pNext;
-            current_this = current->pNext;
-            current = current->pNext;
-            std::cout << "[TAIL]" << " || " << "[pPrev]: " << current->pPrev << " || " << "[ADDRESS]: " << current_this << " || " << "[DATA]: " << data << " || " << "[pNext]:" << current->pNext << std::endl;
-        }
-        else
-        {
-            Node* conteiner_for_uk;
-            Node* current_this;
-            Node* current_prev;
-            conteiner_for_uk = current->pNext;
-            current->pNext = new Node(data, head, conteiner_for_uk);
-            current = current->pNext;
-            current_this = current;
-            current = current->pNext;
-            current->pPrev = current_this;
-            std::cout << "[NODE]" << " || " << "[pPrev]: " << head << " || " << "[ADDRESS]: " << current_this << " || " << "[DATA]: " << data << " || " << "[pNext]:" << conteiner_for_uk << std::endl;
-        }
-    }
-    count_elements++;
-}
-
-void List::adding_element(int place, unsigned long data)
-{
-    Node* current = this->head;
-    int i = 0;
-    if (place == 0) {
-        current->pPrev = new Node(data, nullptr, current);
-        head = current->pPrev;
-    }
-    else {
-        while (place - i != 1) {
-            current = current->pNext;
-            i++;
-        };
-        Node* conteiner_for_uk;
-        conteiner_for_uk = current->pNext;
-        current->pNext = new Node(data, current, conteiner_for_uk);
-        current = current->pNext;
-        conteiner_for_uk = current;
-        current = current->pNext;
-        current->pPrev = conteiner_for_uk;
-    };
-    count_elements++;
-}
-
-void List::print_from_head()
-{
-    Node* current = this->head;
-    for (int i = 0; i < count_elements; i++)
-    {
-        std::cout << "[" << "ELEMENT " << i << "]" << " || " << "[pPrev]:" << current->pPrev << " || " << "[DATA]:" << current->data << " || " << "[pNext]:" << current->pNext;
-        std::cout << std::endl;
-        current = current->pNext;
-    }
-}
-
-void List::print_from_end()
-{
-    Node* current = this->tail;
-    for (int i = count_elements - 1; i != -1; i--)
-    {
-        std::cout << "[" << "ELEMENT " << i << "]" << " || " << "[pPrev]:" << current->pPrev << " || " << " [DATA]:" << current->data << " || " << "[pNext]:" << current->pNext;
-        std::cout << std::endl;
-        current = current->pPrev;
-    }
-}
-
-void List::delete_target_element(int target)
-{
-    Node* current = this->head;
-    int i = 0;
-    if (target == 0) {
-        current = head;
-        head = head->pNext;
-        delete current;
-    }
-    if (target > 0 && target < (count_elements - 1)) {
-        while (i - target != 1) {
-            current = current->pNext;
-            i++;
-        };
-        Node* conteiner_for_uk; Node* uk_to_delete;
-        conteiner_for_uk = current;
-        current = current->pPrev;
-        uk_to_delete = current;
-        current = current->pPrev;
-        current->pNext = conteiner_for_uk;
-        conteiner_for_uk = current;
-        current = current->pNext;
-        current->pPrev = conteiner_for_uk;
-        delete uk_to_delete;
-    };
-    if (target == (count_elements - 1)) {
-        current = tail;
-        tail = tail->pPrev;
-        delete current;
-    }
-    count_elements--;
-}
-
-void List::search_index()
-{
-    Node* temp = head;
-    if (head->pNext != nullptr) {
-        head = head->pNext;
-        head->pPrev = nullptr;
-        delete temp;
-        count_elements--;
-    }
-    else
-    {
-        delete head; count_elements--;
-    }
-
-}
-
-void List::cls()
-{
-    while (count_elements)
-    {
-        search_index();
-    }
-}
-
-void List::search_index(unsigned long value)
-{
-    Node* current = this->head;
-    int i = 0;
-    if ((current->data) == value) {
-        std::cout << "[SEARCH INDEX]: " << i;
-    }
-    else {
-        while (((current->data) != value) && (i != count_elements - 1))
-        {
-            current = current->pNext;
-            i++;
-        }
-        if (i != 0)
-            std::cout << "[SEARCH INDEX]: " << i << std::endl;
-    };
-}
-
-unsigned long List::sum()
-{
-    unsigned long sum = 0;
-    Node* current = this->head;
-    while (current->pNext != nullptr)
-    {
-        sum += current->data;
-        current = current->pNext;
-    }
-    return sum;
-}
-
-unsigned long List::mid_value()
-{
-    unsigned long mid;
-    mid = sum() / count_elements;
-    return mid;
-}
-
-unsigned long List::min_value()
-{
-    Node* current = this->head;
-    unsigned long min = current->data;
-    while ((current->pNext) != nullptr)
-    {
-        if (current->data < min)
-            min = current->data;
-        current = current->pNext;
-    }
-    return min;
-}
-
-unsigned long List::max_value()
-{
-    Node* current = this->head;
-    unsigned long max = current->data;
-    while ((current->pNext) != nullptr)
-    {
-        current = current->pNext;
-        if (current->data > max)
-            max = current->data;
-
-    }
-    return max;
-}
--- ISTbd-22/Tagashev I.I/lab8.1/source.cpp
+++ ISTbd-22/Tagashev I.I/lab8.1/source.cpp
@@ -1,23 +1,58 @@
-п»ї#include <iostream>
+#include <iostream>
 #include "list.h"
-using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    List list;
-    std::cout << "============================== PROGRAMS START ==============================" << std::endl;
-    list.adding_element(1);
-    list.adding_element(2);
-    list.adding_element(44);
-    list.adding_element(1234);
-    list.adding_element(123);
-    list.adding_element(12);
-    std::cout << "============================== LIST OF ELEMENTS ==============================" << std::endl;
-    list.print_from_head();
-    std::cout << "============================== Quantity ==============================" << std::endl;
-    std::cout << "Quantity: " << list.getsize();
-    std::cout << endl;
-    list.cls();
-    return 0;
+	setlocale(LC_ALL, "RUS");
+
+	std::cout << "Тест работы." << std::endl;
+
+	List<int>* list1 = new List<int>;
+	list1->print_list();
+	unsigned long var = 5;
+	unsigned long var2 = 1;
+	unsigned long var3 = 2;
+	unsigned long var4 = 7;
+
+	std::cout << "Добавляем эл-т " << var << std::endl;
+	list1->append(var);
+	std::cout << "Добавляем эл-т " << var2 << std::endl;
+	list1->append(var2);
+	std::cout << "Добавляем эл-т " << var3 << std::endl;
+	list1->append(var3);
+	std::cout << std::endl;
+
+	std::cout << "Печать списка:" << std::endl;
+	list1->print_list();
+	std::cout << std::endl;
+
+	std::cout << "Добавляем эл-т " << var4 << " на третье место" << std::endl;
+	list1->insert(var4, 3);
+
+	std::cout << "Печать списка:" << std::endl;
+	list1->print_list();
+	std::cout << std::endl;
+
+	std::cout << "Удаляем эл-т с индксом " << 2 << std::endl;
+	list1->delete_ind(2);
+
+	std::cout << "Печать списка:" << std::endl;
+	list1->print_list();
+	std::cout << std::endl;
+
+	std::cout << "Печать обратного списка:" << std::endl;
+	list1->print_list_reverse();
+	std::cout << std::endl;
+
+	std::cout << "Индекс искомого эл-та " << var3 << " равен " << list1->search_ind(var3) << std::endl;
+
+	std::cout << "Кол-во эл-ов: " << std::endl << list1->get_size() << std::endl;
+
+	list1->properties_list();
+
+	std::cout << "Удаление списка" << std::endl;
+	list1->delete_front();
+	std::cout << "Печать списка:" << std::endl;
+	list1->print_list();
+
 }
