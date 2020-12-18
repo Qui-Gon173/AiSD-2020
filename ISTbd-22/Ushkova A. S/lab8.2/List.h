b5c9c9a51e387f1c7d58fa77d9c90bfb2feb9e10
LAPTOP-VNO8FJGM\HP <s.ushkva@yandex.ru>
2020-12-18 20:12:35 +0400

    lab 8.2

---
+++ ISTbd-22/Ushkova A. S/lab8.2/List.h
@@ -0,0 +1,272 @@
+#pragma once
+#include <iostream>
+#include "Node.cpp"
+#include "my_iterator.cpp"
+
+using namespace std;
+
+class List
+{
+ public:
+    List();
+    ~List();
+    //добавление в конец
+    void push_back(unsigned int data);
+    //Счетчик
+    int GetSize(){return Size;}
+    //Возврат значение по индексу
+    unsigned int& operator[](const int index);
+    //Добавление элемента в начало списка
+    void push_front(unsigned int data);
+    //Добавление элемента по заданному индексу
+    void insert(unsigned int data, int index);
+    //удаление элемента в списке по указанному индексу
+	void removeAt(int index);
+    //удаление первого элемента в списке
+	void pop_front();
+    //Очистить список
+	void clear();
+    //Вывод индекса по определенному значению
+    void poisk_index(unsigned int value);
+    //Печать списка
+    void printList();
+    //Сумма всех значений
+    unsigned int summ();
+    //Среднее значение
+    unsigned int avarage();
+    //Минимальное значение
+    unsigned int Min();
+    //Максимальное значение
+    unsigned int Max();
+    void Add_for_generator(unsigned int data);
+
+ private:
+        int Size;
+        Node *head;
+        Node *last;
+ public:
+    ////////////////////////////////////////Explosion ZONE
+    //Опрпеделяем типы и методы begin и end
+    typedef my_iterator const_iterator;
+    const_iterator begin() const{
+        return const_iterator(head);
+    };
+    const_iterator end() const{
+        return const_iterator(nullptr);
+    };
+};
+
+List::List() : head(nullptr) 
+{
+    Size=0;
+}
+List::~List()
+{
+}
+
+unsigned int& List::operator[](const int index)
+{
+   int count = 0;
+   Node * current = this->head;
+   while(current!=NULL)
+   {
+       if(count==index)
+       {
+           return current->data;
+       }
+      current = current->pNext;
+      count++;
+   }
+}
+
+void List::push_back(unsigned int data){
+
+   if (head == NULL)
+	{
+		head = new Node(data);
+	}
+	else
+	{
+		Node *current = this->head;
+
+		while (current->pNext != NULL)
+		{
+			current = current->pNext;
+		}
+		current->pNext = new Node(data);
+
+	}
+
+	Size++;
+}
+void List::push_front(unsigned int data)
+{
+	head = new Node(data, head);
+	Size++;
+}
+
+void List::insert(unsigned int data, int index)
+{
+  if (index == 0)
+	{
+		push_front(data);
+	}
+    else
+	{
+		Node *previous = this->head;
+
+		for (int i = 0; i < index - 1; i++)
+		{
+			previous = previous->pNext;
+		}
+
+		Node *newNode = new Node(data, previous->pNext);
+
+		previous->pNext = newNode;
+
+		Size++;
+    }
+  }
+
+void List::pop_front()
+{
+	Node *temp = head;
+
+	head = head->pNext;
+
+	delete temp;
+
+	Size--;
+
+}
+
+void List::removeAt(int index)
+{
+	if (index == 0)
+	{
+		pop_front();
+	}
+	else
+	{
+		Node *previous = this->head;
+		for (int i = 0; i < index - 1; i++)
+		{
+			previous = previous->pNext;
+		}
+
+		
+		Node *toDelete = previous->pNext;
+
+		previous->pNext = toDelete->pNext;
+
+		delete toDelete;
+
+		Size--;
+	}
+}
+
+void List::clear()
+{
+	while (Size)
+	{
+		pop_front();
+	}
+}
+
+void List::poisk_index(unsigned int value)
+ {
+     Node *current = this->head;
+    int i=0;
+    if((current->data)==value)
+    {
+        cout<<"\nThe index of this item: "<<i<<endl;
+    }
+    else
+    {
+        while (((current->data)!=value) && (i!=Size-1))
+        {
+            current=current->pNext;
+            i++;
+        }
+        if(i!=0)
+        cout<<"\nThe index of this item: "<<i<<endl;
+    }
+    
+ }
+    
+void List::printList()
+{
+    Node *current=this->head;
+    cout<<"Print List"<<endl;
+    for(int i=0;i<Size;i++)
+    {
+        cout<<current->data<<' ';
+        current=current->pNext;
+    }
+}
+
+unsigned int List::summ()
+{
+    unsigned int summ = 0;
+    Node *current= this->head;
+    for(int i=0; i<Size; i++)
+    {
+       summ+=current->data;
+       current=current->pNext;
+    }
+    return summ;
+}
+
+unsigned int List::avarage()
+{
+    unsigned int ava;
+    ava= summ()/GetSize();
+    return ava;
+}
+
+unsigned int List::Min()
+{
+    Node *current=this->head;
+    unsigned int min = current->data;
+    for(int i=0; i<Size; i++)
+    {
+       if(current->data<min)
+       min=current->data;
+       current=current->pNext;
+    }
+    return min;
+}
+
+unsigned int List::Max()
+{
+    Node *current=this->head;
+    unsigned int max = current->data;
+    for(int i=0; i<Size; i++)
+    {
+       if(current->data>max)
+       max=current->data;
+       current=current->pNext;
+    }
+    return max;
+}
+
+void List::Add_for_generator(unsigned int data)
+{
+   if (head == NULL)
+	{
+		head = new Node(data);
+	}
+	else
+	{
+		Node *current = this->head;
+
+		while (current->pNext != NULL)
+		{
+			current = current->pNext;
+		}
+		current->pNext = new Node(data);
+
+	}
+
+	Size++; 
+}
---
+++ ISTbd-22/Ushkova A. S/lab8.2/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <algorithm>
+#include "List.h"
+#include <list>
+#include "geometric_generator.cpp"
+
+using namespace std;
+//вывод контейнера
+void print(List& container){
+	cout<<"size: "<<container.GetSize()<<endl;
+	
+	for(auto& item:container){
+		cout<<item<<endl;
+	}
+}
+//вывод суммы контейнера
+void Summ(List& container){
+	//cout<<"size: "<<container.getsize()<<endl;
+	unsigned long sum=0;
+	for(auto& item:container){
+		sum+=item;
+	}
+    cout<<"Sum: "<<sum<<endl;
+}
+//вывод среднего значения контейнера
+void Avarage(List& container){
+	//cout<<"size: "<<container.getsize()<<endl;
+	unsigned long a=0;
+	for(auto& item:container){
+		a+=item;
+	}
+    a=a/container.GetSize();
+    cout<<"Average value of elements: "<<a<<endl;
+}
+
+//вывод минимального значения<algorithm>
+void Min(List& container){
+    auto min=min_element(container.begin(),container.end());
+    cout<<"Minimum element: "<<*min<<endl;
+}
+
+//вывод максимального значения<algorithm>
+void Max(List& container){
+    auto max=max_element(container.begin(),container.end());
+    cout<<"Maximum element: "<<*max<<endl;
+}
+
+//вывод индекса минимального значения<algorithm>
+void MinIndex(List& container){
+
+    auto minIn=min_element(container.begin(),container.end());
+    cout<<"Minimum index element ";
+    container.poisk_index(*minIn);
+}
+
+//вывод индекса максимального значения<algorithm>
+void MaxIndex(List& container){
+
+    auto maxIn=max_element(container.begin(),container.end());
+    cout<<"Maximum index element ";
+    container.poisk_index(*maxIn);
+}
+
+void geometric(List& container,unsigned long first,unsigned long border,int factor){
+	auto generator= geometric_generator(first,border,factor);
+
+	for(auto& item:generator){
+		container.Add_for_generator(item);
+	}
+}
+
+int main()
+{
+//    List lst;
+//    lst.push_front(3);
+//    lst.printList();
+    auto container=new List;
+    container->push_front(1);
+    container->push_front(12);
+    geometric(*container,2,64,2);
+    print(*container);//функция, выводящая контейнер
+    container->clear();//чистка контейнера
+    system("pause");
+    return 0;
+}
