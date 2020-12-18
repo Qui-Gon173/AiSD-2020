e1107dcf7ca3054240aeddc7c0178fda481d5029
DESKTOP-CJ1PUL9\Chekhov <pavel.miridonov123@gmail.com>
2020-12-18 17:37:16 +0400

    Миридонов Павел lab1 fix

--- ISTbd-22/Miridonov-P-A/lab1/main.cpp
+++ ISTbd-22/Miridonov-P-A/lab1/main.cpp
@@ -2,32 +2,26 @@
 #include <conio.h>
 
 using namespace std;
-
-int main()
+void ArrayRandFill(int array[], int n)
 {
-
-    setlocale(LC_ALL, "ru-RU");
-    // задаем массив и числа
-    const int n = 15; // задаем размерность массива
-    int array[n] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
-    int a = 0;
-    //вывод массива
+    srand((unsigned)time(0));
     for (int i = 0; i < n; i++)
     {
-        cout << array[i];
-        cout << " || ";
+        array[i] = rand() % 10;
     }
-    int number = 9; // задаем переменную для поиска нужного числа.
-    if (number > n) // проверка от неправильного ввода числа
-    {
-        cout << "Задайте корректное число!" << endl;
-        return 0;
-    }
-    else {
-    }
+}
+void ArrayOut(int array[], int n)
+{
     for (int i = 0; i < n; i++)
     {
-        if (array[i] == number) // делаем проверку, равен ли наш массив к переменной  got
+        cout << array[i] << " ";
+    }
+}
+int Search(int number, int n, int array[], int a)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] == number) // делаем проверку, равен ли наш массив переменной number
         {
             array[a++] = i;
         }
@@ -36,11 +30,26 @@ int main()
     {
         for (int i = 0; i < a; i++)
         {
-            cout << "\nЧисло " << number << " с индексом в массиве: " << array[i] << endl;
+            cout << "Число " << number << " с индексом в массиве: " << array[i] << endl;
         }
     }
     else {
-        cout << "Массив не найден" << endl;
+        cout << "Число не найдено" << endl;
+        return - 1;
     }
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const int n = 10;
+    int array[n];
+    int a = 0;
+    int number;
+    ArrayRandFill(array,n);
+    cout << "Массив:"<<endl;
+    ArrayOut(array, n);
+    cout << "\nВведите число для поиска" << endl;
+    cin >> number; // считываем число для поиска
+    Search(number,n,array,a);
     return 0;
 }
