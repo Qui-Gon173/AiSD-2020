eecf682199d3785579868f5b47bfb77492cc7396
alekseevnagirl <kashitsyna.2001@mail.ru>
2021-01-04 18:28:37 +0400

    Добавила 6 лабораторную

---
+++ ISTbd-21/KashitsynaNA/lab6/Source1.cpp
@@ -0,0 +1,20 @@
+// Выбор данных из памяти с помощью разных указателей
+// Использование функций приведения типов
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	unsigned long L = 12345678;
+	char* cp = (char *)&L;
+	int* ip = (int *)&L;
+	long* lp = (long *)&L;
+
+	cout << "\n&L = " << &L;
+	cout << "\nL = " << L;
+	cout << "\n*cp = " << *cp;
+	cout << "\n*ip = " << *ip;
+	cout << "\n*lp = " << *lp;
+
+	return 0;
+}
---
+++ ISTbd-21/KashitsynaNA/lab6/Source2.cpp
@@ -0,0 +1,58 @@
+//Операции над указателями
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	int* ca, * cb;
+	int* sa, * sb, * sc;
+
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+
+	c = a + b;
+
+	sb = &b;//инициализация указателей через взятие адреса
+	sa = &a;
+	sc = &c;
+
+	//присваивание указателю значения выражения
+	*sc = c + a - b;
+	ca = sa;//присваивание указателю значения другого указателя
+	cb = sb;
+	*sa = 12;//присваивание указателю значения константы
+
+	cout << "\n*ca = " << *ca;
+	cout << "\n*sa = " << *sa;
+	cout << "\n*cb = " << *cb;
+	cout << "\n*sb = " << *sb;
+	cout << "\n*sc = " << *sc;
+
+	cout << "\nca = " << ca;
+	cout << "\ncb = " << cb;
+	cout << "\nsc = " << sc;
+
+	cout << "\na = " << a;
+	cout << "\nb = " << b;
+	cout << "\nc = " << c;
+
+	cout << "\n&a = " << &a;
+	cout << "\n&b = " << &b;
+	cout << "\n&c = " << &c;
+
+	cout << "\n*&a = " << *&a;
+
+	cout << "\n*cb-*ca = " << *cb - *ca;
+	cout << "\n*cb+*ca = " << *cb + *ca;
+
+	*cb += 2;
+	cout << "\ncb = " << cb;
+	cb++; 
+	cout << "\ncb = " << cb;
+	ca--; 
+	cout << "\ncа = " << ca;
+	cout << "\ncb-ca = " << cb - ca << endl;
+
+	system("pause");
+	return 0;
+}
