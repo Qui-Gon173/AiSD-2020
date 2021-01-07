a49ab06967c183a6c91b8217eece86dec45fd09a
alekseevnagirl <kashitsyna.2001@mail.ru>
2021-01-07 13:54:42 +0400

    Добавила лабораторную 8.1

---
+++ ISTbd-21/KashitsynaNA/lab8.1/lab8.1.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <stdlib.h>
+#include <locale.h>
+#include <windows.h>
+#include "list.h"
+#include "list.cpp"
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::cout << "Односвязный линейный список" << std::endl << "************************************" << std::endl;
+
+    List* Spisok = new List;
+    Spisok->Insert(0, 0); Spisok->Insert(3, 1); Spisok->Insert(-58, 2); Spisok->Insert(-6527, 4); Spisok->Insert(489, 3);
+
+    std::cout << "Список:" << std::endl;
+    Spisok->Print(); 
+
+    std::cout << "" << std::endl << std::endl << "Добавьте элемент, выбрав для него позицию" << std::endl;
+    long value;
+    std::cout << "Элемент: ";
+    std::cin >> value;
+    int value_index;
+    std::cout << "Позиция элемента: ";
+    std::cin >> value_index;
+    Spisok->Insert(value, value_index);
+    std::cout << "Элемент успешно добавлен в список" << std::endl << std::endl; 
+
+    std::cout << "Измененный список:" << std::endl;
+    Spisok->Print(); 
+
+    std::cout << "" << std::endl << std::endl << "Удалите элемент с позиции" << std::endl;
+    long delete_index;
+    std::cout << "Позиция элемента: ";
+    std::cin >> delete_index;
+    Spisok->Delete(delete_index);
+    std::cout << "Элемент успешно удален из списка" << std::endl << std::endl;
+
+    std::cout << "Измененный список:" << std::endl;
+    Spisok->Print();
+
+    std::cout << "" << std::endl << std::endl << "Получите индекса элемента по значению" << std::endl;
+    long value_poisk;
+    std::cout << "Элемент: ";
+    std::cin >> value_poisk;
+    Spisok->Index(value_poisk);
+
+    std::cout << "" << std::endl << "Количество элементов в списке: ";
+    Spisok->GetCount();
+
+    std::cout << "" << std::endl << "Сумма всех значений: ";
+    Spisok->Summa();
+
+    std::cout << "" << std::endl << "Среднее значение: ";
+    Spisok->Srednee();
+
+    std::cout << "" << std::endl << "Значение минимального элемента: ";
+    Spisok->Min();
+
+    std::cout << "" << std::endl << "Значение максимального элемента: ";
+    Spisok->Max();
+    std::cout << "" << std::endl;
+
+    std::cout << "" << std::endl << "Чтобы очистить список, нажмите 1: ";
+    int och;
+    std::cin >> och;
+    if (och == 1)
+    {
+        Spisok->Clear();
+        std::cout << "Список полностью очищен" << std::endl << std::endl;
+    }
+    std::cout << "Измененный список:" << std::endl;
+    Spisok->Print();
+    
+
+    return 0;
+}
+
+
---
+++ ISTbd-21/KashitsynaNA/lab8.1/list.h
@@ -0,0 +1,211 @@
+#pragma once
+#include <iostream>
+
+
+class List;
+class Elem
+{
+	long data; // данные экземпляра
+	Elem* Next;
+	friend class List;
+
+public:
+	Elem(long data = NULL, Elem* Next = NULL)
+	{
+		data = 0;
+		Next = 0;
+	};
+};
+
+class List
+{
+		Elem* Head;
+		Elem* Tail;
+		int Count; //количество элементов
+
+	public:
+		List()
+		{
+			Head = 0;
+			Tail = 0;
+			Count = 0;
+
+		}
+
+
+		//функция добавления
+		void Add(long data)
+		{
+			Elem* temp = new Elem(data, NULL);
+			if (Head != NULL) //Если список не пуст
+			{
+				Tail->Next = temp; //Указываем адрес следующего за хвостом элемента
+				Tail = temp;
+				Count += 1; //Меняем адрес хвоста
+			}
+			else //Если список пустой
+			{
+				//Предыдущий элемент указывает в пустоту
+				Head = Tail = temp;
+			}
+		};
+
+
+		//Функция добавления элемента на позицию
+		void Insert(long data, int index)
+		{
+			if (index <= 0 && !(this->Head)) {
+				Add(data); ++(this->Count); return;
+			}
+			Elem* tf = this->Head;
+			for (int i = 1; i < index && tf->Next; ++i)
+				tf = tf->Next;
+			Elem* temp = new Elem;
+			temp->data = data;
+			temp->Next = tf->Next;
+			tf->Next = temp;
+			++(this->Count);
+		}
+
+
+		//Функция удаления
+		void Delete(int x) //x - позиция удаляемого элемента
+		{
+			if ((Head != NULL) && (x < Count) && (x >= 0))
+			{
+				Elem* temp = Head;
+				Elem* helping = Head;
+
+				for (int i = 0; i < x; i++)
+				{
+					helping = temp; // предыдущее значение temp
+					temp = temp->Next;
+				}
+
+				if (temp == Head) // если элемент который надо удалить первый
+				{
+					Head = temp->Next;
+				}
+				else
+				{
+					helping->Next = temp->Next;
+				}
+				free(temp);
+				Count--; // уменьшаем размер списка
+			}
+		};
+
+		//Функция получения индекса элемента по значению
+		void Index(long element)
+		{
+			Elem* current = this->Head;
+			int ind = 0;
+			if ((current->data) == element)
+			{
+				std::cout << "Индекс элемента: " << ind << std::endl;
+			}
+			else
+			{
+				while (((current->data) != element) && (ind != Count - 1))
+				{
+					current = current->Next;
+					ind++;
+				}
+				if (ind != 0)
+					std::cout << "Индекс элемента: " << ind << std::endl;
+			}
+		};
+
+
+		//Функция вывода на экран
+		void Print()
+		{
+			if (!Head) { std::cout << "Список пуст" << std::endl; }
+			else {
+				Elem* temp = Head;
+				do {
+					std::cout << temp->data << " "; // вывод значения элемента p
+					temp = temp->Next; // переход к следующему узлу
+				} while (temp != NULL);
+			}
+		};
+
+
+		// Получить количество
+		void GetCount()
+		{
+		std::cout << Count;
+		};
+
+
+		//Сумма всех значений
+		void Summa()
+		{
+			long sum = 0;
+			Elem* current = this->Head;
+			for (int i = 0; i < Count; i++)
+			{
+				sum = sum + current->data;
+				current = current->Next;
+			}
+			std::cout << sum;
+		};
+		
+
+		//Среднее значение
+		void Srednee()
+		{
+			long sum = 0;
+			Elem* current = this->Head;
+			for (int i = 0; i < Count; i++)
+			{
+				sum = sum + current->data;
+				current = current->Next;
+			}
+			int sr = sum / Count;
+			std::cout << sr;	
+		};
+
+
+		//Значение минимального элемента
+		void Min()
+		{
+			Elem* current = this->Head;
+			long minim = current->data;
+			for (int i = 0; i < Count; i++)
+			{
+				if (current->data < minim)
+					minim = current->data;
+				current = current->Next;
+			}
+			std::cout << minim;
+		};
+
+
+		//Значение максимального элемента
+		void Max()
+		{
+			Elem* current = this->Head;
+			long maxim = current->data;
+			for (int i = 0; i < Count; i++)
+			{
+				if (current->data > maxim)
+					maxim = current->data;
+				current = current->Next;
+			}
+			std::cout << maxim;
+		};
+
+
+		//Полная очистка контейнера
+		void Clear()
+		{
+			while (Head) {
+				Delete(0);
+			}
+		};
+};
+
+
+
+
