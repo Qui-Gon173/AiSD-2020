8415b99b42bb5e2244401b87ebf434f488a674fe
Emilia <mishacollins7300@gmail@com>
2021-01-10 17:45:34 +0400

    lab 7 is ready

---
+++ ISTbd-21/MyravevaEA/lab 7/аисд лаба 7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void Bashni(int m, int a, int b, int c)
+{
+    if (m == 0) {
+        return;
+    }
+    Bashni(m - 1, a, c, b);
+    cout << a << " => " << b << endl;
+    Bashni(m - 1, c, b, a);
+}
+
+int  main()
+{
+    int  amount = 5, basic = 1, finite = 2, other = 3;
+    Bashni(amount, basic, other, finite);
+    return 0;
+}
