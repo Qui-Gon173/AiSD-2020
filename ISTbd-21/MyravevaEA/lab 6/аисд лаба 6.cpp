feef3159006bfd11dc11b85c219e2ea1a8efd5a4
Emilia <mishacollins7300@gmail@com>
2021-01-10 17:27:57 +0400

    lab 6 is ready

---
+++ ISTbd-21/MyravevaEA/lab 6/аисд лаба 6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	unsigned long L = 12345678;
+	char* cp = (char*)&L;
+	int* ip = (int*)&L;
+	long* lp = (long*)&L;
+
+	cout << "\n&L = " << &L;
+	cout << "\nL = " << L;
+	cout << "\n*cp = " << *cp;
+	cout << "\n*ip = " << *ip;
+	cout << "\n*lp = " << *lp;
+
+	return 0;
+}
