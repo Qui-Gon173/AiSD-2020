ed297ca8a94ebc751505b4d26af465a27dd6037e
Evtushenko-Alex <Evtushenko8sasha4mn@yandex.ru>
2021-01-11 13:24:26 +0400

    Сразу до проверки исправил лабу. Код писал по лекции.

--- ISTbd-21/Evtushenko A. A/lab_7/main.cpp
+++ ISTbd-21/Evtushenko A. A/lab_7/main.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
-
 using namespace std;
 
-int tower_Of_Hanoi(int nDisks, char A, char B, char C)
+void move_tower(int in, int to, int n)
 {
-  int nMoves;
-  if (nDisks == 1)
-  {
-    cout << endl << A << " -> " << B;
-    nMoves = 1;
-  }
-  else
-  {
-    nMoves = tower_Of_Hanoi(nDisks - 1, A, C, B);
-    cout << endl << A << " -> " << B;
-    nMoves++;
-    nMoves += tower_Of_Hanoi(nDisks - 1, C, B, A);
-  }
-  return nMoves;
+    if (n == 1)
+    {
+        cout << in << " -> " << to << endl;
+    }
+    else
+    {
+        int free = 6 - (in + to);
+        move_tower(in, free, n - 1);
+        move_tower(in, to, 1);
+        move_tower(free, to, n - 1);
+    }
 }
 
 int main()
 {
-  char a='A', b='B', c='C';
-  int nDisks, nMoves;
-  cout << "Enter the number of disks: ";
-  cin >> nDisks;
-  nMoves = tower_Of_Hanoi(nDisks, a, b, c);
-  cout << endl << endl << nDisks << " disks have been moved around " << nMoves << " times.";
-  return 0;
+    int n;
+    cout << "n = "; cin >> n;
+    move_tower(1, 3, n);
+    cin.get();
+    cin.get();
 }
