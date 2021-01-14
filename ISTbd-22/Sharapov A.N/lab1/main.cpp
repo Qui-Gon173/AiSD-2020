b35005a341a8a14275a35ad96a01f9ac4395c4f7
JuzuuS <JuzuuS@users.noreply.github.com>
2021-01-14 14:37:41 +0400

    lab1

---
+++ ISTbd-22/Sharapov A.N/lab1/main.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+
+using namespace std;
+
+int barrier_search(int* mArray, int _size, int find_el) {
+    int position = 0;
+    if (mArray[_size - 1] != find_el) {
+        mArray[_size - 1] = find_el;
+        for (; mArray[position] != find_el; position++);
+        position++;
+    }
+
+    else return _size;
+
+    return position < _size ? position : 0;
+}
+
+
+int main() {
+
+    int ar[] = { 1, 4 , 4, 5, 6, 3, 2, 7 };
+    cout << barrier_search(ar, 8, 7) << endl;
+    system("pause");
+    return 0;
+}
