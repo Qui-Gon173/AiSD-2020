02da05d0d47c7b2f6066c89e38fb57dcf4f6d674
ilias-19 <as.ilyas2001@mail.ru>
2021-01-14 23:24:41 +0400

    lab-7

---
+++ ISTbd-22/Akchurin I.R/lab 7/lab 7.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void HanoiTower(int num, char from, char to, char buff)
+{
+    if (num != 0)
+    {
+        HanoiTower(num - 1, from, buff, to);
+        cout << from << " -> " << to << endl;
+        HanoiTower(num - 1, buff, to, from);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int N;
+    cout << "Введите количество дисков на стержне А:" << endl;
+    cin >> N;
+    HanoiTower(N, 'A', 'B', 'C');
+    return 0;
+}
